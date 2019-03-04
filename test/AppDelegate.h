47e6682c0c0163c8b2545061ef429951f3e12e57
袁树峰 <yuansf@corp.hxqc.com>
2019-03-04 17:02:42 +0800

    00

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by cpr287 on 2019/3/4.
+//  Copyright © 2019 cpr287. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by cpr287 on 2019/3/4.
+//  Copyright © 2019 cpr287. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
