974956d63cc83f76218245e79c9e3cc2e34c1445
袁树峰 <yuansf@corp.hxqc.com>
2019-03-04 17:12:16 +0800

    0

---
+++ test/Common.h
@@ -0,0 +1,17 @@
+//
+//  Common.h
+//  test
+//
+//  Created by cpr287 on 2019/3/4.
+//  Copyright © 2019 cpr287. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Common : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
